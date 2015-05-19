c6363450e1ae0d05f54950eb9871e4e8463c67a1
Isagani Mendoza <gani.mendoza@gmail.com>
2015-05-19 09:59:56 +0800

    Added type MapSI

--- sigfunc.go
+++ sigfunc.go
@@ -1,24 +1,25 @@
 package sigfunc
 
-//License: MIT
-//Copyright (c) 2015 Isagani Mendoza (itjumpstart.wordpress.com)
+//Author: Gani Mendoza (itjumpstart.wordpress.com)
 
 //Function signature for reading, writing and processing data (usually JSON)
 //A read or write is a process (DAL, DML, etc)
 //Business logic is also a process, etc.
+
+type MapSI map[string]interface{}
 
 type P00 interface {
 	Process00()
 }
 
 type P01 interface {
-	Process01() map[string]interface{}
+	Process01() MapSI
 }
 
 type P10 interface {
-	Process10(map[string]interface{})
+	Process10(MapSI)
 }
 
 type P11 interface {
-	Process11(map[string]interface{}) map[string]interface{}
+	Process11(MapSI) MapSI
 }
