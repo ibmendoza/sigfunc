c7beee5d7ab729307e63d21792f7279b9e4267c5
Gani Mendoza <gani.mendoza@gmail.com>
2015-05-19 09:23:25 +0800

    First commit

---
+++ sigfunc.go
@@ -0,0 +1,23 @@
+package sigfunc
+
+//Author: Gani Mendoza (itjumpstart.wordpress.com)
+
+//Function signature for reading, writing and processing data (usually JSON)
+//A read or write is a process (DAL, DML, etc)
+//Business logic is also a process, etc.
+
+type P00 interface {
+	Process00()
+}
+
+type P01 interface {
+	Process01() map[string]interface{}
+}
+
+type P10 interface {
+	Process10(map[string]interface{})
+}
+
+type P11 interface {
+	Process11(map[string]interface{}) map[string]interface{}
+}
